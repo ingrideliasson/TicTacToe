57b424ba1c5473be98f302a0a89aadc3a2bddb1e
majaw <majaw@Mac.lan>
2025-08-13 13:17:30 -0700

    Fix color of buttons

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -4,20 +4,20 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold mb-8">Tic Tac Toe</h1>
-      <p> Choose game mode:</p>
+      <h1 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text">Tic Tac Toe</h1>
+      <p class="m-8 bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text"> Choose game mode:</p>
       <div className="flex space-x-4">
         <Link
           to="/aiboard"
-          className="px-6 py-3 bg-green-500 text-white rounded-lg shadow hover:bg-green-600 transition"
+          className="bg-gradient-to-r from-blue-600 to-green-500 m-8 px-6 py-3 text-white rounded-lg"
         >
           Single Player
         </Link>
         <Link
           to="/board"
-          className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition"
+          className="bg-gradient-to-r from-green-500 to-indigo-400 m-8 px-6 py-3 text-white rounded-lg"
         >
-          Two player
+          Two Player
         </Link>
       </div>
     </div>
