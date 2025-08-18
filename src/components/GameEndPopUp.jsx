966760ca14781c60c4863d949eaff7e1a87fa60c
DennisNevback <dennis.nevback@gmail.com>
2025-08-18 10:34:44 +0200

    Added animation and basic styling

--- src/components/GameEndPopUp.jsx
+++ src/components/GameEndPopUp.jsx
@@ -1,37 +1,44 @@
-import { findAllByTestId } from "@testing-library/dom";
-import {Button} from 'react'
+import {motion} from 'framer-motion'
 
-function GameEndPopUp({ isOpen, setIsOpen, winner, reset, newGame  }) {
+function GameEndPopUp({ isOpen, setIsOpen, winner, reset, newGame }) {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-10">
+    <motion.div
+      initial={{ opacity: 0, y: -10 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        duration: 0.3,
+        delay: .8,
+        ease: "easeOut",
+      }}
+      className="fixed inset-0 flex items-center justify-center z-10">
       {/* Bakgrund */}
       <div className="absolute inset-0 bg-black opacity-50"></div>
 
       {/* Själva popupen */}
-      <div className="relative bg-white p-6 rounded-lg shadow-lg z-11 h-1/4 w-1/4">
-        <button
+      <div className="relative bg-white p-6 rounded-lg shadow-lg z-11 h-[700px] w-[340px]">
+        <button className="font-cherry p-2 px-4 bg-sky-300 text-white rounded-lg disabled:opacity-50 "
           onClick={() => {
             setIsOpen(false);
           }}
         >X</button>
-        <div>
+        <div className="flex justify-center items-center h-1/2 font-cherry text-3xl">
           {winner ? (
             <p>Winner: {winner[0]}!</p>
           ) : (
             <p>Game is a tie!</p>
             )}
         </div>
-        <div>
-          <button
+        <div className="absolute bottom-20 inset-x-0 flex justify-center space-x-4">
+          <button className="font-cherry text-white p-2 px-4 bg-sky-300 rounded-lg disabled:opacity-50"
             onClick={() => {
               reset();
               setIsOpen(false);
             }}
           >Reset</button>
 
-          <button
+          <button className="font-cherry p-2 px-4 bg-sky-300 text-white rounded-lg disabled:opacity-50 "
             onClick={() => {
               newGame();
               setIsOpen(false);
@@ -39,7 +46,7 @@ function GameEndPopUp({ isOpen, setIsOpen, winner, reset, newGame  }) {
           >New Game</button>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
